f048b6c45f89ce8b2f569c873f198eaa7d83f4bb
henry_23 <henry.maoht@outlook.com>
2022-07-12 03:29:33 +0800

    fix(lab-new/02): add more contents

--- src/lab-new/02/assets/hello/hello-1.c
+++ src/lab-new/02/assets/hello/hello-1.c
@@ -3,15 +3,15 @@
 
 int init_module(void)
 {
-  pr_info("Hello world 1.\n");
+	pr_info("Hello world 1.\n");
 
-  /* A non 0 return means init_module failed; module can't be loaded. */
-  return 0;
+	/* A non 0 return means init_module failed; module can't be loaded. */
+	return 0;
 }
 
 void cleanup_module(void)
 {
-  pr_info("Goodbye world 1.\n");
+	pr_info("Goodbye world 1.\n");
 }
 
 MODULE_LICENSE("GPL");
